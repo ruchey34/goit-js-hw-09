c9c48b1cad82b22165fce4b4ffc6d7c9c5ff1798
ruchey34 <me.oleg1@gmail.com>
2022-11-20 17:38:10 +0200

    upd 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,28 @@
-const btnStart = document.querySelector(`[data-start]`);
+const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
-
+let interval = null;
 btnStart.addEventListener('click', startSwitch);
 btnStop.addEventListener('click', stopSwitch);
 
 btnStop.setAttribute('disabled', true);
 
-function getRandomHexColor() {
-    console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 
-function startSwitch(){
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function startSwitch () {
     btnStop.removeAttribute('disabled', false);
     btnStart.setAttribute('disabled', true);
     interval = setInterval(() => {
         body.style.backgroundColor = getRandomHexColor();
-    }, 1000);
-}
+    }, 1000)
+  }
 
-function stopSwitch(){
+
+function stopSwitch () {
     btnStop.setAttribute('disabled', true);
     btnStart.removeAttribute('disabled', false);
     clearInterval(interval);
-}
+  }
